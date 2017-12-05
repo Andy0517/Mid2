f947c175bbf120a0b562a39fb540f56537affbb7
Andy0517 <ad870517@gmail.com>
2017-12-05 11:59:19 +0800

    roll

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,13 +1,28 @@
 package com.fju.mid2;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
-
+    int number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
     }
+    public void roll(View view){
+        number = new Random().nextInt(5)+1;
+        new AlertDialog.Builder(this)
+                .setTitle("Number")
+                .setMessage("your number is:"+number)
+                .setPositiveButton("again",null)
+                .show();
+    }
+
+
 }
